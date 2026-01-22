025d35a32226940112ce22f8f22405e3050f8dd5
VASU CHAND <vasuchand9@gmail.com>
2026-01-22 11:23:20 +0530

    add the array folder

---
+++ array/reverseingroup.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+void reverse(int n, int r, int arr[], int g){
+for(int i=0;i<n/r;i++){
+swap
+}
+
+
+}
+int main(){
+    int n ;
+    cout <<"enter the no of elements in the array"<<endl;
+    cin>>n;
+int arr[n];
+cout<<"enter the elements of the array"<<endl;
+for(int i=0;i<n;i++){
+    cin>>arr[i] ;
+}
+cout << " the elemnts of array are " << endl;
+for(int i=0;i<n;i++){
+    cout << arr[i] << " ";
+}
+cout << " enter the groups in which araay is to be reversed" << endl;
+int r;
+cin >> r;
+int g= n/r;
+reverse(n,g,arr,g);
+
+}
